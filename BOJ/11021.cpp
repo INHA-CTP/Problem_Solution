6837575e27bf64c1a4fffe71ff9f00e480d3adba
san9407 <38039468+san9407@users.noreply.github.com>
2019-03-19 21:39:06 +0900

    Create 11021.cpp

---
+++ BOJ/11021.cpp
@@ -0,0 +1,20 @@
+/*
+	Author : SIYUN LEE
+	Date : 19.03.18
+*/
+
+#include <cstdio>
+using namespace std;
+
+int main()
+{
+	int T;
+	scanf("%d", &T);
+	for (int i = 1; i <= T; i++)
+	{
+		int a, b;
+		scanf("%d %d", &a, &b);
+		printf("Case #%d: %d\n", i, a + b);
+	}
+	return 0;
+}
