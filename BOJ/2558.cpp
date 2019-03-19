72ee39f87b3f09322a1de7fda58b147b2273ac0c
san9407 <38039468+san9407@users.noreply.github.com>
2019-03-19 21:33:31 +0900

    Create 2558.cpp

---
+++ BOJ/2558.cpp
@@ -0,0 +1,15 @@
+/*
+	Author : SIYUN LEE
+	Date : 19.03.18
+*/
+
+#include <cstdio>
+using namespace std;
+
+int main()
+{
+	int a, b;
+	scanf("%d %d", &a, &b);
+	printf("%d\n", a + b);
+	return 0;
+}
